698523d7d0d5f673821b23833a0e94d64d376c96
ISOMARCO <inagiyev@icloud.com>
2023-11-15 01:07:05 +0400

    1

--- config/database.php
+++ config/database.php
@@ -75,8 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
-            #'sslmode' => 'disable'
+            'sslmode' => 'prefer'
         ],
 
         'sqlsrv' => [
