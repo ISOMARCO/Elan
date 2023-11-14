bd86da1390c4e267d780603d4150672383f0ed7b
ISOMARCO <inagiyev@icloud.com>
2023-11-15 01:46:48 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\Ads\AdsController;
+use App\Http\Controllers\AdsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
