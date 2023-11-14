52a58a1ede5f12f009cbfb9435b75e974480da39
ISOMARCO <inagiyev@icloud.com>
2023-11-15 00:40:25 +0400

    1

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 class Users extends Model
 {
     protected  $table = "Users";
