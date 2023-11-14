a6f6d17c7111dafcbeafafedcf6b533b3f099452
ISOMARCO <inagiyev@icloud.com>
2023-11-15 01:58:04 +0400

    1

--- app/Http/Controllers/Ads/Ads1Controller.php
+++ app/Http/Controllers/Ads/Ads1Controller.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 class Ads1Controller extends Controller
 {
+    public function __construct()
+    {
+        echo "AA";
+    }
     public function main()
     {
         echo "OK";
