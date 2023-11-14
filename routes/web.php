a6e0b0f6acfed21d299ca8ed09befb2797147ed4
ISOMARCO <inagiyev@icloud.com>
2023-11-14 23:51:30 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
+#use App\Http\Controllers\HomeController;
+#use App\Http\Controllers\LoginController;
+use App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
