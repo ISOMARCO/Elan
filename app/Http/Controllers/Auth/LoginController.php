45ee7b63b0a8cbb0859bb86ae0d56a136a866491
ISOMARCO <inagiyev@icloud.com>
2023-11-15 16:58:44 +0400

    1

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,22 +1,16 @@
 <?php
 namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
-use App\Models\Auth\Users;
 use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function main()
     {
-
         return view('Frontend.Login.main');
     }
 
     public function loginAction(Request $request) : void
     {
-        if ($request->ajax() || $request->wantsJson()) {
-            dd('ok');
-        } else {
-            abort(403, 'Unauthorized');
-        }
+        echo "OK";
     }
 }
