29742d05365d3eb1e50c1a49052031b990b1b83d
ISOMARCO <inagiyev@icloud.com>
2023-11-15 16:45:58 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'main'])->name('Home');
 Route::get('/home', [HomeController::class, 'main'])->name('Home');
 Route::get('/login', [LoginController::class, 'main'])->name('Login');
+Route::get('/login/loginAction', [LoginController::class, 'loginAction'])->name('LoginAction');
 Route::get('/register', [RegisterController::class, 'main'])->name('Register');
