714edbdb3118cab74bcdd95e99df06ede324f1cd
ISOMARCO <inagiyev@icloud.com>
2023-11-16 01:19:09 +0400

    1

--- app/Http/Controllers/Media/TelegramWebhookController.php
+++ app/Http/Controllers/Media/TelegramWebhookController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\Media;
 
 use App\Http\Controllers\Controller;
-
+use App\Helpers\TelegramBot;
 class TelegramWebhookController extends Controller
 {
     public function main()
     {
-        echo "OK";
+        $telegram = new TelegramBot();
+        $data = $telegram->getData();
+        if(!empty($data))
+        {
+            if(strtolower($data['message']['text']) == 'hello')
+            {
+                $telegram->sendMessage([
+                    'text' => 'Sanada hello gardas'
+                ]);
+            }
+        }
     }
 }
