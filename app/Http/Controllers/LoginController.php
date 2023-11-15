a451caa66bfe6bdaa2b1956fe4567ce9bb5c1224
ISOMARCO <inagiyev@icloud.com>
2023-11-15 11:07:19 +0400

    1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
 class LoginController extends Controller
 {
     public function main()
     {
+        echo Route::is();
         return view('Frontend.Login.main');
     }
 }
