28b7aed9a682623457fe7707e1ccf3bde2add83f
ISOMARCO <inagiyev@icloud.com>
2023-11-15 15:51:57 +0400

    1

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
-
+use App\Models\Auth\Users;
 class LoginController extends Controller
 {
     public function main()
     {
+        $users = new Users();
+        $users->insertUser();
         return view('Frontend.Login.main');
     }
 }
--- app/Models/Auth/Users.php
+++ app/Models/Auth/Users.php
@@ -4,9 +4,16 @@ namespace App\Models\Auth;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Users extends Model
 {
     use HasFactory;
-    protected $table = 'Users';
+    public function insertUser() : void
+    {
+        DB::table('Users')->insert([
+            'Name' => 'Ismayil',
+            'Surname' => 'Nagiyev'
+        ]);
+    }
 }
