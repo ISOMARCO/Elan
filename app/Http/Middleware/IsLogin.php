a1de893ee9ae2ec7d3c3811e7ca5452d6ccbe288
ISOMARCO <inagiyev@icloud.com>
2023-11-15 11:59:22 +0400

    1

--- app/Http/Middleware/IsLogin.php
+++ app/Http/Middleware/IsLogin.php
@@ -18,7 +18,7 @@ class IsLogin
         $session = 1;
         if($session)
         {
-            return redirect()->route('Home');
+            return false;
         }
         return $next($request);
     }
