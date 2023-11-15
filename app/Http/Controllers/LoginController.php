6c8fe4ac792abd5d39104c92e6a2b1ceae3a30d7
ISOMARCO <inagiyev@icloud.com>
2023-11-15 11:11:33 +0400

    1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,7 +6,6 @@ class LoginController extends Controller
 {
     public function main()
     {
-        echo request()->route()->getName()."Controller";
         return view('Frontend.Login.main');
     }
 }
