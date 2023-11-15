5a2d5bba60db89b0ac6c893c103da58cc08d8c52
ISOMARCO <inagiyev@icloud.com>
2023-11-15 11:04:02 +0400

    1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,10 +5,6 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('IsLogin');
-    }
     public function main()
     {
         return view('Frontend.Login.main');
