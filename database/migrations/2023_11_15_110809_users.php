2eeeb468763abb913b1b7d2c5b118cd64bf743b5
ISOMARCO <inagiyev@icloud.com>
2023-11-15 15:13:54 +0400

    1

--- database/migrations/2023_11_15_110809_users.php
+++ database/migrations/2023_11_15_110809_users.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('Users', function (Blueprint $table) {
+            $table->id('Id');
+            $table->string('Name', 50);
+            $table->string('Surname', 50);
+            $table->tinyInteger('Status')->default(0);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::drop('Users');
     }
 };
