dbcec07a828a41612f0c8c8488e0948e003d1924
ISOMARCO <inagiyev@icloud.com>
2023-11-17 00:36:13 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,6 @@ class HomeController extends Controller
 {
     public function main() : string
     {
-        Cache::store('redis')->put('demo', "demo data", 120);
-        echo Cache::store('redis')->get('demo');
         return view('Frontend.Home.main');
     }
 }
