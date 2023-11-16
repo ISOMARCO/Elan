912e42296062563f1e219545c916e1da27b6b3bd
ISOMARCO <inagiyev@icloud.com>
2023-11-16 16:03:25 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
     public function main()
     {
         $telegram = new TelegramBot();
+        print_r($telegram->getWebhookInfo());
         return view('Frontend.Home.main');
     }
 }
