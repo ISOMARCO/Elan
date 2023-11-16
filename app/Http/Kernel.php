767614a26a1073013f444763fccbd0261d4b7957
ISOMARCO <inagiyev@icloud.com>
2023-11-16 17:19:26 +0400

    1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -44,6 +45,9 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'auth' => [
+            \App\Http\Middleware\VerifyCsrfToken::class
+        ]
     ];
 
     /**
