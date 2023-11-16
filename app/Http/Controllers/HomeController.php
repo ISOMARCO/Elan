6b8ad6b1aa650eda03ff2f54ae78a0f62a22c0fa
ISOMARCO <inagiyev@icloud.com>
2023-11-17 00:35:34 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
+
 class HomeController extends Controller
 {
     public function main() : string
     {
+        Cache::store('redis')->put('demo', "demo data", 120);
+        echo Cache::store('redis')->get('demo');
         return view('Frontend.Home.main');
     }
 }
