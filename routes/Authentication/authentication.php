e545e7b5163d993d69f6e54725dde9452bec9595
ISOMARCO <inagiyev@icloud.com>
2023-11-16 17:49:19 +0400

    1

--- routes/Authentication/authentication.php
+++ routes/Authentication/authentication.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authentication\RegisterController;
-
+use App\Http\Controllers\Authentication\LoginController;
 
 Route::get('/register', [RegisterController::class, 'main'])->name('Register');
 Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction']);
