ed7454ef4450a6c1c62ab24baca65850f865f977
ISOMARCO <inagiyev@icloud.com>
2023-11-16 16:42:15 +0400

    1

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        "https://laravel.iso.com.az/public/telegram_webhook"
     ];
 }
