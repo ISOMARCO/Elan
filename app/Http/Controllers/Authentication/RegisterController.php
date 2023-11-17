e039f76357e3ddf3bbb5d1ae0f4bc9ab811660a4
ISOMARCO <inagiyev@icloud.com>
2023-11-17 13:31:46 +0400

    1

--- app/Http/Controllers/Authentication/RegisterController.php
+++ app/Http/Controllers/Authentication/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
 
     public function registerAction(Request $request)
     {
-        if ($request->ajax() || $request->wantsJson())
+        if (!$request->ajax() || $request->wantsJson())
         {
             echo json_encode(['ok' => $request->post('phone_number')]);
         }
