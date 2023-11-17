147278717d79cd0fb674d8b2bb0a6ebc05546e13
ISOMARCO <inagiyev@icloud.com>
2023-11-17 12:05:34 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,24 +6,6 @@ class HomeController extends Controller
 {
     public function main() : string
     {
-        Cache::store('redis')->put('Key', 'Value', 120);
-        if(Cache::store('redis')->has('Key'))
-        {
-            echo "12";
-        }
-        else
-        {
-            echo "16";
-        }
-        Cache::store('redis')->pull('Key');
-        if(Cache::store('redis')->has('Key'))
-        {
-            echo "21";
-        }
-        else
-        {
-            echo "25";
-        }
         return view('Frontend.Home.main');
     }
 }
