1771177bbb6fc176534049761eacdc0533b592d4
ISOMARCO <inagiyev@icloud.com>
2023-11-17 12:02:22 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Cache;
-
 class HomeController extends Controller
 {
     public function main() : string
     {
-        #Cache::store('redis')->put('Key', 'Value', 120);
-        if(Cache::store('redis')->has('Key'))
-        {
-            echo "var";
-        }
-        else
-        {
-            echo "yox";
-        }
+
         return view('Frontend.Home.main');
     }
 }
