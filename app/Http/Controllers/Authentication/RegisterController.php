a873cb67ffdd468eae4de7546d29140faf5c33de
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-11-19 17:53:48 +0400

    1

--- app/Http/Controllers/Authentication/RegisterController.php
+++ app/Http/Controllers/Authentication/RegisterController.php
@@ -13,7 +13,7 @@ class RegisterController extends Controller
         return view('Frontend.Login.main');
     }
 
-    public function registerAction(Request $request) : Json
+    public function registerAction(Request $request) : int
     {
         if ($request->ajax() || $request->wantsJson())
         {
