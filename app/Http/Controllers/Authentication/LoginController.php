89282d84e2e91f5bbedf25038805c2ee37097181
ISOMARCO <inagiyev@icloud.com>
2023-11-21 23:32:14 +0400

    1

--- app/Http/Controllers/Authentication/LoginController.php
+++ app/Http/Controllers/Authentication/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers\Authentication;
 use App\Http\Controllers\Controller;
+use App\Models\Authentication\Users;
 use Illuminate\Http\Request;
 class LoginController extends Controller
 {
@@ -13,6 +14,25 @@ class LoginController extends Controller
     {
         if ($request->ajax() || $request->wantsJson())
         {
+            $email_or_phone = $request->post('email_or_phone');
+            $password = $request->post('password');
+            $users = new Users();
+            $checkEmpty = [
+                'email_or_phone' => $email_or_phone,
+                'password' => $password
+            ];
+            $emptyErrorArray = [];
+            foreach($checkEmpty as $key => $value)
+            {
+                if($value == NULL)
+                {
+                    $emptyErrorArray[$key] = $users->String_Replace($key)." boş buraxıla bilməz";
+                }
+            }
+            if(count($emptyErrorArray) > 0)
+            {
+                return response()->json(['error' => $emptyErrorArray, 'location' => 'LoginController@loginAction@34'], 422);
+            }
             return response()->json(['error' => $request->post('email_or_phone')], 200);
         }
         else
