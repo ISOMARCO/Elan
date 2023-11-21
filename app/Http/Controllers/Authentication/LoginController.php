e1000be71894693fc7169f9e1cb83e4801df0d20
ISOMARCO <inagiyev@icloud.com>
2023-11-22 00:08:01 +0400

    1

--- app/Http/Controllers/Authentication/LoginController.php
+++ app/Http/Controllers/Authentication/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
             {
                 if($value == NULL)
                 {
-                    return response()->json(['error' => ['email_or_phone' => $users->String_Replace('email_or_phone')]], 422);
+                    return response()->json(['error' => ['email_or_phone' => $users->String_Replace(':email_or_phone')]], 422);
                     $emptyErrorArray[$key] = $users->String_Replace($key)." boş buraxıla bilməz";
                 }
             }
