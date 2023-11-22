85ab79c5b924452cf20bf1bca02ea3dedaa9a4ac
ISOMARCO <inagiyev@icloud.com>
2023-11-23 02:10:39 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ class HomeController extends Controller
 {
     public function main() : string
     {
+        echo Session::get('id');
         Cache::store('redis')->get('user_info_'.Session::get('id'));
         return view('Frontend.Home.main');
     }
