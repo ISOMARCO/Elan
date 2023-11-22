10b9b194ae72c9682d59e235eebb28cad40de454
ISOMARCO <inagiyev@icloud.com>
2023-11-23 01:19:47 +0400

    1

--- resources/views/Frontend/Login/main.blade.php
+++ resources/views/Frontend/Login/main.blade.php
@@ -220,7 +220,7 @@
                         $("small").hide();
                             $.each(errorResponse.error, function (index, value)
                             {
-                                console.log(index+" "+value);
+                                alert(index);
                                 if(index == 'show_alert')
                                 {
                                     Swal.fire({
