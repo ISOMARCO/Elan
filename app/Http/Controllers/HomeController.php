2b993a4948ac8590ff939cad838f16d3dfe80d52
ISOMARCO <inagiyev@icloud.com>
2023-11-23 00:01:36 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ class HomeController extends Controller
 {
     public function main() : string
     {
-        print_r(DB::table('Users')->where('Email', '=', 'inagiyev@icloud.com')->where('Password', '=', 'ismayil')->get());
+        print_r(DB::table('Users')->where('Email', '=', 'inagiyev@icloud.com')->where('Password', '=', 'ismayil')->get()->Name);
         return view('Frontend.Home.main');
     }
 }
