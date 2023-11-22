189ae1b89a319247fae445ef38da86ea873cc270
ISOMARCO <inagiyev@icloud.com>
2023-11-23 02:20:26 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,15 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Session;
 class HomeController extends Controller
 {
     public function main() : string
     {
-        $user = Cache::store('redis')->get('user_info_'.Session::get('id'));
-        echo $user->Name;
         return view('Frontend.Home.main');
     }
 }
