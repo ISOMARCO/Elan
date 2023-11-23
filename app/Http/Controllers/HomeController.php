17098caf2e64f0d6b05e37e0574d28e8057a59c6
nikoo_musa_111 <nikoomusa111@gmail.com>
2023-11-23 18:05:26 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ class HomeController extends Controller
 {
     public function main() : string
     {
-        echo Session::has('id');
+        //echo Session::has('id');
         return view('Frontend.Home.main');
     }
 }
