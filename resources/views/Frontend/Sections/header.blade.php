de4a1737eac849c3f7c3f615196a10ba0607bd9c
ISOMARCO <inagiyev@icloud.com>
2023-11-24 01:40:56 +0400

    1

--- resources/views/Frontend/Sections/header.blade.php
+++ resources/views/Frontend/Sections/header.blade.php
@@ -171,11 +171,26 @@
 </aside>
 <nav class="mobile-nav">
     <div class="container">
-        <div class="mobile-group"><a href="index.html" class="mobile-widget"><i
-                    class="fas fa-home"></i><span>home</span></a><a href="{{url('/login')}}" class="mobile-widget"><i
-                    class="fas fa-user"></i><span>Login</span></a><a href="ad-post.html" class="mobile-widget plus-btn"><i
-                    class="fas fa-plus"></i><span>Ad Post</span></a><a href="notification.html" class="mobile-widget"><i
-                    class="fas fa-bell"></i><span>notify</span><sup>0</sup></a><a href="message.html" class="mobile-widget"><i
-                    class="fas fa-envelope"></i><span>message</span><sup>0</sup></a></div>
+        <div class="mobile-group">
+            <a href="index.html" class="mobile-widget">
+                <i class="fas fa-home"></i><span>home</span>
+            </a>
+            @if(!Session::has('id'))
+                <a href="{{url('/login')}}" class="mobile-widget">
+                    <i class="fas fa-user"></i><span>Login</span>
+                </a>
+            @endif
+            <a href="ad-post.html" class="mobile-widget plus-btn">
+                <i class="fas fa-plus"></i><span>Ad Post</span>
+            </a>
+            <a href="notification.html" class="mobile-widget">
+                <i class="fas fa-bell"></i><span>notify</span>
+                <sup>0</sup>
+            </a>
+            <a href="message.html" class="mobile-widget">
+                <i class="fas fa-envelope"></i><span>message</span>
+                <sup>0</sup>
+            </a>
+        </div>
     </div>
 </nav>
