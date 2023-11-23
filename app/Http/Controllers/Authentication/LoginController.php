0688ac4bcb889ae5df52a8a1dd6ab677b0dabca3
nikoo_musa_111 <nikoomusa111@gmail.com>
2023-11-23 16:31:47 +0400

    1

--- app/Http/Controllers/Authentication/LoginController.php
+++ app/Http/Controllers/Authentication/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends Controller
 
     public function logout()
     {
-        Session::flush();
+        session()->flush();
         return redirect()->back();
     }
 }
