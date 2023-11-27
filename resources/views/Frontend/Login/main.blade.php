ab92914b6ba0c01f4a4eb0767b27915c0b3e3954
ISOMARCO <inagiyev@icloud.com>
2023-11-27 13:23:22 +0400

    1

--- resources/views/Frontend/Login/main.blade.php
+++ resources/views/Frontend/Login/main.blade.php
@@ -172,7 +172,7 @@
                         $("small").hide();
                             Swal.fire({
                                 title: "",
-                                text: e.success,
+                                text: '<i class="fas fa-spinner fa-spin fa-lg" id="login_spinner" style="--fa-primary-color: #4b0aff; --fa-secondary-color: #4b0aff; display: none"></i>'+e.success,
                                 icon: 'success'
                             }).then((result) => {
                                 if (result.isConfirmed) {
