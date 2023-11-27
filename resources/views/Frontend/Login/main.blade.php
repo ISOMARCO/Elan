10840dfe787a493d84d0c6bee04d2a2845b5547d
ISOMARCO <inagiyev@icloud.com>
2023-11-27 13:26:10 +0400

    1

--- resources/views/Frontend/Login/main.blade.php
+++ resources/views/Frontend/Login/main.blade.php
@@ -172,9 +172,8 @@
                         $("small").hide();
                             Swal.fire({
                                 title: '<i class="fas fa-spinner fa-spin fa-lg" id="login_spinner" style="--fa-primary-color: #4b0aff; --fa-secondary-color: #4b0aff; display: none"></i>',
-                                text: '<i class="fas fa-spinner fa-spin fa-lg" id="login_spinner" style="--fa-primary-color: #4b0aff; --fa-secondary-color: #4b0aff; display: none"></i>'+e.success,
-                                icon: 'success',
-                                html: true
+                                html: '<i class="fas fa-spinner fa-spin fa-lg" id="login_spinner" style="--fa-primary-color: #4b0aff; --fa-secondary-color: #4b0aff; display: none"></i>'+e.success,
+                                icon: 'success'
                             }).then((result) => {
                                 if (result.isConfirmed) {
                                     window.location.href = "{{url('/')}}";
