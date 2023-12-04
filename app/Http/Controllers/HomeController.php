5c3a321386e3fb16293a90744b3091b2546d46cc
ISOMARCO <inagiyev@icloud.com>
2023-12-04 13:12:17 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Cookie;
-use App\Helpers\Functions;
-
 class HomeController extends Controller
 {
     public function main() : string
     {
-        echo encrypt('Hello');
+        echo decrypt('eyJpdiI6Ik42L2lMeWltNUkrR3NRV25rc2x6T3c9PSIsInZhbHVlIjoiTlJRdGpLMnZyRmFkaVVWQ3A4TE9Tdz09IiwibWFjIjoiNjZkMTM4YjBlMTQzOGQ4MGMzODMzNGU4NGFkMWQ5N2YwNzQ2NTkwYjkyMWY0YTlhZTI0MDdhMDgyZmVlMjI5NiIsInRhZyI6IiJ9');
 
         //echo decrypt('Hello');
         return view('Frontend.Home.main');
