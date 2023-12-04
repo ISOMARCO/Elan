ef4b1286967cc8c98007b48e0ea54bb0763493fc
ISOMARCO <inagiyev@icloud.com>
2023-12-04 13:10:44 +0400

    1

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -1,19 +1,2 @@
 <?php
 
-function Encrypt($string)
-{
-    $method = "AES-256-CBC";
-    $secret_key = hash("sha256","@!kernuherbve@!_");
-    $secret_iv = '23-+=*#_';
-    $iv = substr(hash('sha256', $secret_iv), 0, 16);
-    return openssl_encrypt($string,$method,$secret_key,false,$iv);
-}
-
-function Decrypt($string)
-{
-    $method = "AES-256-CBC";
-    $secret_key = hash("sha256","@!kernuherbve@!_");
-    $secret_iv = '23-+=*#_';
-    $iv = substr(hash('sha256', $secret_iv), 0, 16);
-    return openssl_decrypt($string,$method,$secret_key,false,$iv);
-}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,9 @@ class HomeController extends Controller
 {
     public function main() : string
     {
-        echo Cookie::has(Decrypt('Remember_Me_Token'));
+        echo encrypt('Hello');
+
+        //echo decrypt('Hello');
         return view('Frontend.Home.main');
     }
 }
