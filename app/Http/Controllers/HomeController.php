f614834ee3a6e0e04cb7a8b8f6fcf083b6ddaca4
ISOMARCO <inagiyev@icloud.com>
2023-12-04 14:04:47 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
     public function main() : string
     {
         $cookie = Cookie::make('Hello', '1', 10);
-        return response()->view('Frontend.Home.main')->withCookie($cookie);
+        echo $cookie->getValue();
+        return view('Frontend.Home.main');
     }
 }
