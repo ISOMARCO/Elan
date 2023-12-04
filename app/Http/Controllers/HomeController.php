b091a3fe804cf27cd03888c7268146753b728f00
ISOMARCO <inagiyev@icloud.com>
2023-12-04 14:48:37 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Cookie;
 class HomeController extends Controller
 {
     public function main(Request $request) : string
