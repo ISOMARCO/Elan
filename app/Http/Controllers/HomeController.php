13f8ad762f8dedaccf115a51f1c924e080b506bb
ISOMARCO <inagiyev@icloud.com>
2023-12-04 14:32:04 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HomeController extends Controller
 {
-    public function main() : string
+    public function main(Request $request) : string
     {
+        $cookie = Cookie::make('hello', '1', 10);
+        $response =  new Response();
+        $response->withCookie($cookie);
+        echo $request->cookie('hello', 'yoxdu');
         return view('Frontend.Home.main');
     }
 }
