c1275b3efc9606cacdab08846aab543129258b0c
ISOMARCO <inagiyev@icloud.com>
2023-12-04 16:53:58 +0400

    1

--- app/Http/Middleware/PullFromGit.php
+++ app/Http/Middleware/PullFromGit.php
@@ -15,7 +15,7 @@ class PullFromGit
      */
     public function handle(Request $request, Closure $next): Response
     {
-        echo "OK";
+        echo $request->cookie(encrypt('Remember_Me_Token'));
         exec('git pull https://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@github.com/ISOMARCO/Elan.git');
         return $next($request);
     }
