d3e971c206605ec722df6cdfcf1bbb2345c0365f
ISOMARCO <inagiyev@icloud.com>
2023-12-06 03:17:09 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,6 @@ class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
-        echo $request->cookie(encrypt('Remember_Me_Token'), 'yoxdu');
         return view('Frontend.Home.main');
     }
 }
