310ae68cb3695d8e616a5ac8585c1514ddf45b52
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-12-09 17:52:53 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,8 @@ class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
+        Cookie::queue(Cookie::make(encrypt('deneme'), '123', (60*24*365)));
+        echo $request->cookie(encrypt('deneme'));
         return view('Frontend.Home.main');
     }
 }
