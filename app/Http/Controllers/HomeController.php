1af6aaeb2d683140e5e5de434485dd51f263291b
ISOMARCO <ismayilnagiyev100@gmail.com>
2023-12-09 18:12:54 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,10 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cookie;
 class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
-        Cookie::queue(Cookie::forget('deneme'));
-        echo request()->cookie('deneme');
         return view('Frontend.Home.main');
     }
 }
