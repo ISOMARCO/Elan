0e95594a8c7dda9f2148478293242e666dac2ece
ISOMARCO <inagiyev@icloud.com>
2024-01-01 14:18:30 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,6 @@ class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
-        Cookie::queue(Cookie::make('Remember_Me_Token', uniqid(), 100));
-        echo Cookie::get('Remember_Me_Token')."<br>";
-        echo Cookie::has('Remember_Me_Token');
         return view('Frontend.Home.main');
     }
 }
