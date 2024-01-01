9d488dff7926f9d4f6be676c55c2276575da5f72
ISOMARCO <inagiyev@icloud.com>
2024-01-01 13:04:54 +0400

    1

--- app/Http/Middleware/AutoLogin.php
+++ app/Http/Middleware/AutoLogin.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
 class AutoLogin
@@ -15,6 +17,10 @@ class AutoLogin
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if(!Session::has('id') && Cookie::has(encrypt('Remember_Me_Token')))
+        {
+            echo "Auto login olmalidi";
+        }
         return $next($request);
     }
 }
