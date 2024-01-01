aa8eaf6725ddc6c61a20d285ae50d8f620e7b97d
ISOMARCO <inagiyev@icloud.com>
2024-01-01 14:14:18 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,7 @@ class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
-        Cookie::queue(Cookie::make('name', 'value', 100));
-        print_r(Cookie::getQueuedCookies());
+        Cookie::make('name', 'value', 100);
         echo Cookie::get('name');
         return view('Frontend.Home.main');
     }
