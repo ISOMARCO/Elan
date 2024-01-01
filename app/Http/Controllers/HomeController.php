d3b150a83140176fd1d10965aa2af9792b788fcf
ISOMARCO <inagiyev@icloud.com>
2024-01-02 03:50:52 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,6 @@ class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
-        DB::table('Users')->truncate();
-        print_r(DB::table('Users')->get());
         return view('Frontend.Home.main');
     }
 }
