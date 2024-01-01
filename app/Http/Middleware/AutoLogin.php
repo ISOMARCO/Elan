c4f2cde5f77c194e65150d89ee103171ed779d77
ISOMARCO <inagiyev@icloud.com>
2024-01-01 13:08:09 +0400

    1

--- app/Http/Middleware/AutoLogin.php
+++ app/Http/Middleware/AutoLogin.php
@@ -17,6 +17,7 @@ class AutoLogin
      */
     public function handle(Request $request, Closure $next): Response
     {
+        echo Cookie::has(encrypt('Remember_Me_Token'))." token";
         if(!Session::has('id') && Cookie::has(encrypt('Remember_Me_Token')))
         {
             echo "Auto login olmalidi";
