2e7d5f0d02f9c0f085c4a0890c4f2f8ce0021c8c
ISOMARCO <inagiyev@icloud.com>
2024-01-03 22:00:43 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+#use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
-        print_r(DB::table('Users')->get());
+        #print_r(DB::table('Users')->get());
         return view('Frontend.Home.main');
     }
 }
