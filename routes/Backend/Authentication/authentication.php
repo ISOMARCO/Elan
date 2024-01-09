fd86d47cf4b77c8e5a750926e08fc0477af894cb
ISOMARCO <inagiyev@icloud.com>
2024-01-09 14:13:51 +0400

    1

---
+++ routes/Backend/Authentication/authentication.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*Route::get('/register', [RegisterController::class, 'main'])->name('Register');
+Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction']);
+Route::get('/login', [LoginController::class, 'main'])->name('Login');
+Route::match(['post', 'get'], '/register/registerAction', [RegisterController::class, 'registerAction']);
+Route::get('/logout', [LoginController::class, 'logout'])->name('Logout');*/
