449dd52f3ae7915b4598fc6443a4d964cb903c07
ISOMARCO <inagiyev@icloud.com>
2024-01-09 14:09:14 +0400

    1

---
+++ resources/views/Backend/Login/Sections/head.blade.php
@@ -0,0 +1,25 @@
+<title>Metronic - The World's #1 Selling Bootstrap Admin Template - Metronic by KeenThemes</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:locale" content="az-AZ">
+<meta property="og:type" content="article">
+<link rel="shortcut icon" href="{{asset('Assets/Backend/media/logos/favicon.ico')}}">
+
+<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+<link href="{{asset('Assets/Backend/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('Assets/Backend/css/style.bundle.css')}}" rel="stylesheet" type="text/css">
+<!--end::Global Stylesheets Bundle-->
+
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-37564768-1');
+</script>
+<!--end::Google tag-->
+<script>
+    // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking)
+    if (window.top != window.self) {
+        window.top.location.replace(window.self.location.href);
+    }
+</script>
---
+++ resources/views/Backend/Login/main.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @include('Backend.Login.Sections.head')
+</head>
+<body id="kt_body" class="app-blank bgi-size-cover bgi-attachment-fixed bgi-position-center">
+    @include('Backend.Login.Sections.header')
+</body>
+<footer>
+    @include('Backend.Login.Sections.footer')
+</footer>
+</html>
