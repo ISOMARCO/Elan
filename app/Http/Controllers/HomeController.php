89c886abda94b6c249bfe130d7c46bef7d185a25
ISOMARCO <inagiyev@icloud.com>
2024-01-09 11:13:33 +0400

    1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
         $method = 'print';
         $telegram = new TelegramActions();
         //echo call_user_func([$telegram, $method]);
-        echo $telegram->{$method}();
+        //echo $telegram->{$method}();
         #print_r(DB::table('Users')->get());
         return view('Frontend.Home.main');
     }
