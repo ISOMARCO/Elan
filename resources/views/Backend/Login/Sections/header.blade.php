de83516d4a2975b15a056c336f113f04954ed0a8
ISOMARCO <inagiyev@icloud.com>
2024-01-09 23:47:27 +0400

    1

--- resources/views/Backend/Login/Sections/header.blade.php
+++ resources/views/Backend/Login/Sections/header.blade.php
@@ -111,11 +111,6 @@
                         <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                             <div></div>
 
-                            <!--begin::Link-->
-                            <a href="reset-password.html" class="link-primary">
-                                Forgot Password ?
-                            </a>
-                            <!--end::Link-->
                         </div>
                         <!--end::Wrapper-->
 
@@ -133,93 +128,12 @@
                         </div>
                         <!--end::Submit button-->
 
-                        <!--begin::Sign up-->
-                        <div class="text-gray-500 text-center fw-semibold fs-6">
-                            Not a Member yet?
-
-                            <a href="sign-up.html" class="link-primary">
-                                Sign up
-                            </a>
-                        </div>
-                        <!--end::Sign up-->
                     </form>
                     <!--end::Form-->
 
                 </div>
                 <!--end::Wrapper-->
 
-                <!--begin::Footer-->
-                <div class=" d-flex flex-stack">
-                    <!--begin::Languages-->
-                    <div class="me-10">
-                        <!--begin::Toggle-->
-                        <button class="btn btn-flex btn-link btn-color-gray-700 btn-active-color-primary rotate fs-base" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="0px, 0px">
-                            <img data-kt-element="current-lang-flag" class="w-20px h-20px rounded me-3" src="{{asset('Assets/Backend/media/flags/united-states.svg')}}" alt="">
-
-                            <span data-kt-element="current-lang-name" class="me-1">English</span>
-
-                            <i class="ki-duotone ki-down fs-5 text-muted rotate-180 m-0"></i>                        </button>
-                        <!--end::Toggle-->
-
-                        <!--begin::Menu-->
-                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4 fs-7" data-kt-menu="true" id="kt_auth_lang_menu">
-                            <!--begin::Menu item-->
-                            <div class="menu-item px-3">
-                                <a href="#" class="menu-link d-flex px-5" data-kt-lang="English">
-                                    <span class="symbol symbol-20px me-4">
-                                        <img data-kt-element="lang-flag" class="rounded-1" src="{{asset('Assets/Backend/media/flags/united-states.svg')}}" alt="">
-                                    </span>
-                                    <span data-kt-element="lang-name">English</span>
-                                </a>
-                            </div>
-                            <!--end::Menu item-->
-                            <!--begin::Menu item-->
-                            <div class="menu-item px-3">
-                                <a href="#" class="menu-link d-flex px-5" data-kt-lang="Spanish">
-                                    <span class="symbol symbol-20px me-4">
-                                        <img data-kt-element="lang-flag" class="rounded-1" src="{{asset('Assets/Backend/media/flags/spain.svg')}}" alt="">
-                                    </span>
-                                    <span data-kt-element="lang-name">Spanish</span>
-                                </a>
-                            </div>
-                            <!--end::Menu item-->
-                            <!--begin::Menu item-->
-                            <div class="menu-item px-3">
-                                <a href="#" class="menu-link d-flex px-5" data-kt-lang="German">
-                                    <span class="symbol symbol-20px me-4">
-                                        <img data-kt-element="lang-flag" class="rounded-1" src="{{asset('Assets/Backend/media/flags/germany.svg')}}" alt="">
-                                    </span>
-                                    <span data-kt-element="lang-name">German</span>
-                                </a>
-                            </div>
-                            <!--end::Menu item-->
-                            <!--begin::Menu item-->
-                            <div class="menu-item px-3">
-                                <a href="#" class="menu-link d-flex px-5" data-kt-lang="Japanese">
-                                    <span class="symbol symbol-20px me-4">
-                                        <img data-kt-element="lang-flag" class="rounded-1" src="{{asset('Assets/Backend/media/flags/japan.svg')}}" alt="">
-                                    </span>
-                                    <span data-kt-element="lang-name">Japanese</span>
-                                </a>
-                            </div>
-                            <!--end::Menu item-->
-                            <!--begin::Menu item-->
-                            <div class="menu-item px-3">
-                                <a href="#" class="menu-link d-flex px-5" data-kt-lang="French">
-                                    <span class="symbol symbol-20px me-4">
-                                        <img data-kt-element="lang-flag" class="rounded-1" src="{{asset('Assets/Backend/media/flags/france.svg')}}" alt="">
-                                    </span>
-                                    <span data-kt-element="lang-name">French</span>
-                                </a>
-                            </div>
-                            <!--end::Menu item-->
-                        </div>
-                        <!--end::Menu-->
-                    </div>
-                    <!--end::Languages-->
-
-                </div>
-                <!--end::Footer-->
             </div>
             <!--end::Content-->
         </div>
