ab5c34494246c69eba42d3c1dd0d15d93806ade8
ISOMARCO <inagiyev@icloud.com>
2024-01-09 10:40:26 +0400

    1

---
+++ app/Helpers/TelegramActions.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Helpers;
+
+class TelegramActions extends TelegramBot
+{
+    public function print()
+    {
+        return "OKEY";
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Helpers\TelegramActions;
 use Illuminate\Http\Request;
 #use Illuminate\Support\Facades\DB;
 
@@ -8,6 +9,8 @@ class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
+        $telegramActions = new TelegramActions();
+        echo $telegramActions->print();
         #print_r(DB::table('Users')->get());
         return view('Frontend.Home.main');
     }
