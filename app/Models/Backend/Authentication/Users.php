7b3660490049909e4ccfe1b95fa8b1ed986cb915
root <root@vmi1570124.contaboserver.net>
2024-02-01 01:42:16 +0400

    1

---
+++ app/Models/Backend/Authentication/Users.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Backend\Authentication;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    use HasFactory;
+}
