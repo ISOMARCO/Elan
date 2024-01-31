359126950338ea6d1568eb8d737bd268260ac60e
ISOMARCO <inagiyev@icloud.com>
2024-01-31 22:04:30 +0400

    1

--- resources/views/Backend/Login/main.blade.php
+++ resources/views/Backend/Login/main.blade.php
@@ -30,82 +30,42 @@
             </div>
             <!--end::Content-->
         </div>
-        <!--begin::Aside-->
-
-        <!--begin::Body-->
         <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
-            <!--begin::Wrapper-->
             <div class="bg-body d-flex flex-column flex-center rounded-4 w-md-600px p-10">
-                <!--begin::Content-->
                 <div class="d-flex flex-center flex-column align-items-stretch h-lg-100 w-md-400px">
-                    <!--begin::Wrapper-->
                     <div class="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
 
-                        <!--begin::Form-->
                         <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form">
-                            <!--begin::Heading-->
                             <div class="text-center mb-11">
-                                <!--begin::Title-->
                                 <h1 class="text-gray-900 fw-bolder mb-3">
                                     Admin panelə giriş
                                 </h1>
-                                <!--end::Title-->
-
-                                <!--begin::Subtitle-->
                                 <div class="text-gray-500 fw-semibold fs-6">
                                     Elan
                                 </div>
-                                <!--end::Subtitle--->
                             </div>
-                            <!--begin::Heading-->
 
-                            <!--begin::Input group--->
                             <div class="fv-row mb-8">
-                                <!--begin::Email-->
-                                <div class="form-group">
-                                    <input type="password" class="form-control" placeholder="Password" name="password">
-                                    <button class="form-icon"><i class="eye fas fa-eye"></i></button>
-                                    <small class="form-alert">Deneme</small>
-                                </div>
-{{--                                <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent">--}}
-{{--                                <div class="alert alert-danger">Mesaj</div>--}}
-                                <!--end::Email-->
+                                <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent">
                             </div>
 
-                            <!--end::Input group--->
                             <div class="fv-row mb-3">
-                                <!--begin::Password-->
                                 <input type="password" placeholder="Şifrə" name="password" autocomplete="off" class="form-control bg-transparent">
-                                <!--end::Password-->
                             </div>
-                            <!--end::Input group--->
 
-                            <!--begin::Wrapper-->
                             <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                                 <div></div>
-
                             </div>
-                            <!--end::Wrapper-->
 
-                            <!--begin::Submit button-->
                             <div class="d-grid mb-10">
                                 <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
                                     <!--begin::Indicator label-->
                                     <span class="indicator-label">Giriş</span>
-                                    <!--end::Indicator label-->
-                                    <!--begin::Indicator progress-->
                                     <span class="indicator-progress">Zəhmət olmasa gözləyin...    <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                                 </button>
-                                    <!--end::Indicator progress-->
                             </div>
-                            <!--end::Submit button-->
-
                         </form>
-                        <!--end::Form-->
-
                     </div>
-                    <!--end::Wrapper-->
-
                 </div>
                 <!--end::Content-->
             </div>
