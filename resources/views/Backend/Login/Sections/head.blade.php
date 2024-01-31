0640c7f7820c09594b587237d4f46a070769d477
ISOMARCO <inagiyev@icloud.com>
2024-01-31 13:56:52 +0400

    1

--- resources/views/Backend/Login/Sections/head.blade.php
+++ resources/views/Backend/Login/Sections/head.blade.php
@@ -1,4 +1,4 @@
-<title>Admin Login</title>
+<title>Admin Login2</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta property="og:locale" content="az-AZ">
