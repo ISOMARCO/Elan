270c12296f9a844ded3bc34548859aeeae2c4639
ISOMARCO <inagiyev@icloud.com>
2024-02-01 12:06:34 +0400

    1

--- app/Exceptions/Backend/Authentication/Authentication.php
+++ app/Exceptions/Backend/Authentication/Authentication.php
@@ -6,8 +6,10 @@ use Exception;
 
 class Authentication extends Exception
 {
+    public $code = 500;
     public function __construct($statusCode = NULL, $message = NULL)
     {
+        $this->code = $statusCode;
         if($message == NULL)
         {
             $message = $this->errorCodes($statusCode);
@@ -15,13 +17,25 @@ class Authentication extends Exception
         parent::__construct($message, $statusCode);
     }
 
-    public function errorCodes($code = 500) : String
+    protected function errorCodes() : String
     {
         $codes = [
             '1000' => 'Email adresi düzgün yazın',
             '1001' => 'Bütün xanaları doldurmalısınız'
         ];
-        if(isset($codes[$code]))
+        if(isset($codes[$this->code]))
+        {
+            return $codes[$this->code];
+        }
+        return "";
+    }
+
+    public function reportErrorCodes() : String
+    {
+        $codes = [
+
+        ];
+        if(isset($codes[$this->code]))
         {
             return $codes[$code];
         }
