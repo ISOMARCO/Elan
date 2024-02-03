250bee5ae107fbbdf7ecc07d7fd76108c4e0070a
ISOMARCO <inagiyev@icloud.com>
2024-02-03 18:01:53 +0400

    1

--- resources/views/Backend/Login/main.blade.php
+++ resources/views/Backend/Login/main.blade.php
@@ -99,7 +99,7 @@
                     error: function(x)
                     {
                         var errorResponse = x.responseJSON || x.responseText;
-                        console.log(x);
+                        console.log(x.status);
                         Swal.fire({
                             title: "ERROR",
                             text: "error var "+errorResponse.error,
