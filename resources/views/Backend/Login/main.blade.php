5ba1e1f704a95d6125b231ee8faadbbbd4d886ea
ISOMARCO <inagiyev@icloud.com>
2024-02-03 19:05:08 +0400

    1

--- resources/views/Backend/Login/main.blade.php
+++ resources/views/Backend/Login/main.blade.php
@@ -100,7 +100,7 @@
                     {
                         var errorResponse = x.responseJSON || x.responseText;
                         Swal.fire({
-                            title: '<i class="fa-solid fa-envelope" style="font-size: 30px;"></i>',
+                            title: '',
                             text: errorResponse.error,
                             icon: 'error'
                         });
