89e004ee85eddbf18753c7ca130a5baa7f3dbf19
ISOMARCO <inagiyev@icloud.com>
2024-02-03 17:51:40 +0400

    1

--- app/Http/Controllers/Backend/Authentication/LoginController.php
+++ app/Http/Controllers/Backend/Authentication/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
             }
             catch(Authentication $e)
             {
-                return response()->json(['success' => "Bu bir errordu"], 200);
+                return response()->json(['error' => $e->getMessage()], 422);
             }
         }
         abort(403, 'Unauthorized');
