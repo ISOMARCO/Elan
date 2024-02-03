dd5b24f36e5d423d7cc17e2b9a47528de42a63f4
ISOMARCO <inagiyev@icloud.com>
2024-02-04 03:16:21 +0400

    1

---
+++ resources/views/Backend/Sections/head.blade.php
@@ -0,0 +1,39 @@
+<title>Admin Panel</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:locale" content="en_US">
+<meta property="og:type" content="article">
+<meta property="og:title" content="Metronic - The World's #1 Selling Bootstrap Admin Template - Metronic by KeenThemes">
+<meta property="og:url" content="https://keenthemes.com/metronic">
+<meta property="og:site_name" content="Metronic by Keenthemes">
+<link rel="canonical" href="https://preview.keenthemes.com/metronic8">
+<link rel="shortcut icon" href="{{asset('Assets/Backend/media/logos/favicon.ico')}}">
+
+<!--begin::Fonts(mandatory for all pages)-->
+<link rel="stylesheet" href="../../../../css-1?family=Inter:300,400,500,600,700">        <!--end::Fonts-->
+
+<!--begin::Vendor Stylesheets(used for this page only)-->
+<link href="{{asset('Assets/Backend/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css">
+<!--end::Vendor Stylesheets-->
+
+
+<!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
+<link href="{{asset('Assets/Backend/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css">
+<link href="{{asset('Assets/Backend/css/style.bundle.css')}}" rel="stylesheet" type="text/css">
+<!--end::Global Stylesheets Bundle-->
+
+<!--begin::Google tag-->
+<script async="" src="../../../../gtag/js-1?id=UA-37564768-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-37564768-1');
+</script>
+<!--end::Google tag-->
+<script>
+    // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking)
+    if (window.top != window.self) {
+        window.top.location.replace(window.self.location.href);
+    }
+</script>
