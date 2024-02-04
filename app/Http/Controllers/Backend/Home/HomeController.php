89e1b7d551bd053fd3a0b180cc683d163f7dc899
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-04 19:08:58 +0400

    1

--- app/Http/Controllers/Backend/Home/HomeController.php
+++ app/Http/Controllers/Backend/Home/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Backend\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use function App\Http\Controllers\Backend\view;
-
 class HomeController extends Controller
 {
     public function main(Request $request)
