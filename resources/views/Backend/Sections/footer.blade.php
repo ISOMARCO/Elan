3e8027b1221d33a864f8b4b663e9b76776ffa643
ISOMARCO <inagiyev@icloud.com>
2024-02-08 15:38:30 +0400

    1

---
+++ resources/views/Backend/Sections/footer.blade.php
@@ -0,0 +1,22 @@
+<!--begin::Global Javascript Bundle(mandatory for all pages)-->
+<script src="{{asset('Assets/Backend/plugins/global/plugins.bundle.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/scripts.bundle.js')}}"></script>
+<!--end::Global Javascript Bundle-->
+
+<!--begin::Vendors Javascript(used for this page only)-->
+<script src="{{asset('Assets/Backend/plugins/custom/datatables/datatables.bundle.js')}}"></script>
+<!--end::Vendors Javascript-->
+
+<!--begin::Custom Javascript(used for this page only)-->
+<script src="{{asset('Assets/Backend/js/custom/pages/user-profile/general.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/widgets.bundle.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/widgets.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/apps/chat/chat.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/upgrade-plan.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/create-app.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/offer-a-deal/type.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/offer-a-deal/details.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/offer-a-deal/finance.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/offer-a-deal/complete.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/offer-a-deal/main.js')}}"></script>
+<script src="{{asset('Assets/Backend/js/custom/utilities/modals/users-search.js')}}"></script>
