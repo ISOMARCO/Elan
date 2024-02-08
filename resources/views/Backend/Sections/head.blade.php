f63cf5e2cd0b6241d4f02e990896532296c0a1ca
ISOMARCO <inagiyev@icloud.com>
2024-02-08 15:40:39 +0400

    1

--- resources/views/Backend/Sections/head.blade.php
+++ resources/views/Backend/Sections/head.blade.php
@@ -10,7 +10,7 @@
 <link rel="shortcut icon" href="{{asset('Assets/Backend/media/logos/favicon.ico')}}">
 
 <!--begin::Fonts(mandatory for all pages)-->
-<link rel="stylesheet" href="../../../../css-1?family=Inter:300,400,500,600,700">        <!--end::Fonts-->
+{{--<link rel="stylesheet" href="../../../../css-1?family=Inter:300,400,500,600,700">        <!--end::Fonts-->--}}
 
 <!--begin::Vendor Stylesheets(used for this page only)-->
 <link href="{{asset('Assets/Backend/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css">
@@ -23,17 +23,4 @@
 <!--end::Global Stylesheets Bundle-->
 
 <!--begin::Google tag-->
-<script async="" src="../../../../gtag/js-1?id=UA-37564768-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-37564768-1');
-</script>
-<!--end::Google tag-->
-<script>
-    // Frame-busting to prevent site from being loaded within a frame without permission (click-jacking)
-    if (window.top != window.self) {
-        window.top.location.replace(window.self.location.href);
-    }
-</script>
+{{--<script async="" src="../../../../gtag/js-1?id=UA-37564768-1"></script>--}}
