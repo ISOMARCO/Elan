9c0ed8fad13faa7a6552edf76ae30d164d95c721
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-10 00:52:33 +0400

    1

--- resources/views/Backend/Users/main.blade.php
+++ resources/views/Backend/Users/main.blade.php
@@ -30,7 +30,7 @@
                         <h3 class="card-title">Fixed Header Table</h3>
 
                         <div class="card-tools">
-                            <div class="input-group input-group-sm" style="width: 150px;">
+                            <div class="input-group input-group-sm">
                                 <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
 
                                 <div class="input-group-append">
@@ -42,7 +42,7 @@
                         </div>
                     </div>
                     <!-- /.card-header -->
-                    <div class="card-body table-responsive p-0" style="height: 300px;">
+                    <div class="card-body table-responsive p-0">
                         <table class="table table-head-fixed text-nowrap">
                             <thead>
                             <tr>
