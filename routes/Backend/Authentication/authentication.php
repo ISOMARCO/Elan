92606ece8d16644151e93ca24fa801c0c9dc4a1e
ISOMARCO <inagiyev@icloud.com>
2024-02-09 08:29:51 +0400

    1

--- routes/Backend/Authentication/authentication.php
+++ routes/Backend/Authentication/authentication.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Authentication\LoginController;
 Route::get('/admin/login', [LoginController::class, 'main'])->name("Login");
