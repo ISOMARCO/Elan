aca2a7b9316803b82cb93ef963c90296734ebf1e
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-09 21:00:39 +0400

    1

--- app/Http/Controllers/Frontend/Home/HomeController.php
+++ app/Http/Controllers/Frontend/Home/HomeController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers\Frontend\Home;
 use App\Http\Controllers\Controller;
-use http\Client\Request;
 
 class HomeController extends Controller
 {
-    public function main(Request $request) : string
+    public function main() : string
     {
         return view('Frontend.Home.main');
     }
