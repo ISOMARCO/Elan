64e5d707ff62ca4b376381d3844454069d8c5867
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-09 20:57:57 +0400

    1

--- app/Http/Controllers/Frontend/Home/HomeController.php
+++ app/Http/Controllers/Frontend/Home/HomeController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Frontend\Home;
-#use App\Helpers\TelegramActions;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -11,11 +10,6 @@ class HomeController extends Controller
 {
     public function main(Request $request) : string
     {
-        //$method = 'print';
-        //$telegram = new TelegramActions();
-        //echo call_user_func([$telegram, $method]);
-        //echo $telegram->{$method}();
-        #print_r(DB::table('Users')->get());
         return view('Frontend.Home.main');
     }
 }
