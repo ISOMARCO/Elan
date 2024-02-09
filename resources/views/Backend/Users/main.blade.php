5523667514f40aa5ed920e76aec5854324bf0e4d
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-10 01:01:22 +0400

    1

--- resources/views/Backend/Users/main.blade.php
+++ resources/views/Backend/Users/main.blade.php
@@ -42,7 +42,7 @@
                         </div>
                     </div>
                     <!-- /.card-header -->
-                    <div class="card-body table-responsive p-0" style="height: calc(100vh - 5%); overflow: auto;">
+                    <div class="card-body table-responsive p-0" style="height: calc(100vh - 10%); overflow: auto;">
                         <table class="table table-head-fixed text-nowrap">
                             <thead>
                             <tr>
