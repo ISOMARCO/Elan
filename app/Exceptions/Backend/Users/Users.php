a788ef088ef4657fa0370fb3ded83e637e723a21
ISOMARCO <inagiyev@icloud.com>
2024-02-09 15:56:54 +0400

    1

--- app/Exceptions/Backend/Users/Users.php
+++ app/Exceptions/Backend/Users/Users.php
@@ -6,5 +6,37 @@ use Exception;
 
 class Users extends Exception
 {
-    //
+    public function __construct($statusCode = NULL, $message = NULL)
+    {
+        $this->code = $statusCode;
+        if($message == NULL)
+        {
+            $message = $this->errorCodeMessage();
+        }
+        parent::__construct($message, $statusCode);
+    }
+
+    protected function errorCodeMessage() : String
+    {
+        $codes = [
+            '2000' => 'Email adresi düzgün yazın'
+        ];
+        if(isset($codes[$this->code]))
+        {
+            return $codes[$this->code];
+        }
+        return "ERROR";
+    }
+
+    protected function reportErrorCodes() : String
+    {
+        $codes = [
+            '5000'
+        ];
+        if(isset($codes[$this->code]))
+        {
+            return $codes[$this->code];
+        }
+        return "";
+    }
 }
