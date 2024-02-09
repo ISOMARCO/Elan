a8140fb1a45e66bc50937eb6822b4fc94e0c9e8d
ISOMARCO <inagiyev@icloud.com>
2024-02-09 08:49:30 +0400

    1

--- app/Http/Middleware/Backend/IsLogin.php
+++ app/Http/Middleware/Backend/IsLogin.php
@@ -16,7 +16,7 @@ class IsLogin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Session::has('id') && $request->route()->getName() !== 'Logout')
+        if(!Session::has('id') && $request->route()->getName() !== 'Logout')
         {
             abort(403, 'Olmaz');
         }
