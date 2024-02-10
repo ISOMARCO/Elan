e2385d3d561ede14729799883c862a3e411cb9e1
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-10 15:22:48 +0400

    1

--- resources/views/Backend/Users/main.blade.php
+++ resources/views/Backend/Users/main.blade.php
@@ -26,7 +26,7 @@
         @foreach($userList as $value)
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">{{$value->Name}} {{$value->Surname}}</h3>
+                <h3 class="card-title">İstifadəçi nömrəsi: {{sprintf("%08d", $value->Id)}}</h3>
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                         <i class="fas fa-minus"></i>
@@ -37,11 +37,35 @@
                 </div>
             </div>
             <div class="card-body">
-                Start creating your amazing application!
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Ad</th>
+                            <th>Soyad</th>
+                            <th>Email</th>
+                            <th>Telefon nömrəsi</th>
+                            <th>Qeydiyyat tarixi</th>
+                            <th>Son giriş tarixi</th>
+                            <th>Cins</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{$value->Name}}</td>
+                            <td>{{$value->Surname}}</td>
+                            <td>{{$value->Email}}</td>
+                            <td>{{$value->Phone_Number}}</td>
+                            <td>{{$value->Registration_Date}}</td>
+                            <td>{{$value->Last_Login_Date}}</td>
+                            <td>{{$value->Gender}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <!-- /.card-body -->
             <div class="card-footer">
-                Footer
+                <button type="button" class="btn btn-outline-primary" style="float: left;"><i class="fas fa-edit"></i></button>
+                <button type="button" class="btn btn-outline-danger" style="float: right;"><i class="fas fa-ban"></i></button>
             </div>
             <!-- /.card-footer-->
         </div>
