c225d73790dd7d22a09a5a48619d869d212ce2a4
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-10 15:41:55 +0400

    1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Blade::directive('Date_To_String', function ($expression) {
-            return "<?php echo \\Carbon\\Carbon::parse({$expression})->isoFormat('LL LTS'); ?>";
+        \Blade::directive('Date_To_String', function ($date, $explode = ' ') {
+            $date = explode(" ", $date);
+            $dateFormat = explode('-', $date[0]);
+            $months = [
+                '01' => 'Yanvar',
+                '02' => 'Fevral',
+                '03' => 'Mart',
+                '04' => 'Aprel',
+                '05' => 'May',
+                '06' => 'İyun',
+                '07' => 'İyul',
+                '08' => 'Avqust',
+                '09' => 'Sentyabr',
+                '10' => 'Oktyabr',
+                '11' => 'Noyabr',
+                '12' => 'Dekabr'
+            ];
+            $return = $dateFormat[2]." ".$months[$dateFormat[1]]." ".$dateFormat[0].$explode.$date[1];
+            return "<?php echo $return; ?>";
         });
     }
 }
