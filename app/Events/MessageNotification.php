958fd867e2af25401def17a58b4d947f0c4fdbce
ISOMARCO <inagiyev@icloud.com>
2024-02-11 02:03:57 +0400

    1

--- app/Events/MessageNotification.php
+++ app/Events/MessageNotification.php
@@ -10,16 +10,16 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageNotification
+class MessageNotification implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-
+    public $message;
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct($message)
     {
-        //
+        $this->message = $message;
     }
 
     /**
@@ -30,7 +30,7 @@ class MessageNotification
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new Channel('notification'),
         ];
     }
 }
