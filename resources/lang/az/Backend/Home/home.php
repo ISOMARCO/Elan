60bc1f2004ddb479041f7b55784abf8e4a7b3164
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-12 02:05:04 +0400

    1

--- resources/lang/az/Backend/Home/home.php
+++ resources/lang/az/Backend/Home/home.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'home' => 'Ana səhifə22'
+    'home' => 'Ana səhifə'
 ];
--- resources/views/Backend/Home/main.blade.php
+++ resources/views/Backend/Home/main.blade.php
@@ -15,7 +15,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">{{ __('Backend.Home.home.home') }}</a></li>
+                        <li class="breadcrumb-item"><a href="#">@lang('Backend.Home.home.home')</a></li>
                         <li class="breadcrumb-item active">Blank Page</li>
                     </ol>
                 </div>
