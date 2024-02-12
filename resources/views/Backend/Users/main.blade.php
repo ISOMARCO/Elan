a0eb60aee04067d6cffccbcf1797ac41e0db6ae4
ISOMARCO <inagiyev@icloud.com>
2024-02-12 13:36:26 +0400

    1

--- resources/views/Backend/Users/main.blade.php
+++ resources/views/Backend/Users/main.blade.php
@@ -72,7 +72,7 @@
                     </div>
                     <!-- /.card-body -->
                     <div class="card-footer">
-                        <button type="button" class="btn btn-outline-secondary" style="float: left;"><i class="fas fa-edit"></i></a>
+                        <button type="button" class="btn btn-outline-secondary" style="float: left;"><i class="fas fa-edit"></i></button>
                         <button type="button" class="btn btn-outline-danger" style="float: right;"><i class="fas fa-ban"></i></button>
                     </div>
                     <!-- /.card-footer-->
