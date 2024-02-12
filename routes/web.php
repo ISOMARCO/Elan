d5440d4dffdcd6f1791a7434e8a77b7bd6b194ca
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-12 23:10:13 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\Backend\IsLogin as Backend_IsLogin;
 use App\Http\Controllers\Backend\Home\HomeController as BackendHomeController;
-use App\Http\Controllers\Backend\Users\UsersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,4 +21,3 @@ Route::redirect('/home', '/');
 Route::match(['post', 'get'],'/telegram_webhook', [TelegramWebhookController::class, 'main'])->withoutMiddleware([VerifyCsrfToken::class]);
 Route::middleware([Backend_IsLogin::class])->get('/admin/', [BackendHomeController::class, 'main'])->name('Admin_Home');
 Route::redirect('/admin/home', '/admin/');
-Route::get('/admin/users/user_edit',[UsersController::class, 'user_edit']);
