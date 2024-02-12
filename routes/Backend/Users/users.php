42d459eaef3dd00b88db9b37d9aa7686054b8914
ISOMARCO <inagiyev@icloud.com>
2024-02-12 15:48:55 +0400

    1

--- routes/Backend/Users/users.php
+++ routes/Backend/Users/users.php
@@ -3,4 +3,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Users\UsersController;
 
 Route::get('/admin/users', [UsersController::class, 'main'])->name("Backend_Users");
-Route::get('/admin/edit_user/{id}', [UsersController::class, 'edit_user'])->name('Backend_User_Edit');
+Route::get('/admin/edit_user/{id}', [UsersController::class, 'user_edit'])->name('Backend_User_Edit');
