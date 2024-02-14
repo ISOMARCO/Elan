cda227bd60f21b0365d20546e7e3030ba0133c4d
root <root@vmi1570124.contaboserver.net>
2024-02-15 00:21:41 +0400

    1

---
+++ app/Models/Backend/Users/User_Status_History.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Backend\Users;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User_Status_History extends Model
+{
+    use HasFactory;
+}
