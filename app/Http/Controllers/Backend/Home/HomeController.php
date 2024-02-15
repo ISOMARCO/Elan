42cb97a06e8b25ceb3d0a71e8129bdc5fc2839b0
ISOMARCO <inagiyev@icloud.com>
2024-02-15 13:46:51 +0400

    1

--- app/Http/Controllers/Backend/Home/HomeController.php
+++ app/Http/Controllers/Backend/Home/HomeController.php
@@ -7,6 +7,7 @@ class HomeController extends Controller
 {
     public function main()
     {
+        echo preg_replace('/(\d{1,2})(?=(\d{3})+$)/', '$1,', 10000);
         return view('Backend.Home.main');
     }
 }
