bb62453617e634db81470172016d30b293ae1895
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-17 01:46:27 +0400

    1

---
+++ resources/views/Backend/Users/timeline.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="az">
+<head>
+    @include('Backend.Sections.head')
+</head>
+<body>
+    @include('Backend.Sections.header')
+
+    @include('Backend.Sections.footer')
+</body>
+</html>
