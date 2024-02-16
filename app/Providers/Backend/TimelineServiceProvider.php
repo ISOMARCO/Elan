2fce575406535b7a6c49731b8630e2eab435f9e1
root <root@vmi1570124.contaboserver.net>
2024-02-16 16:03:02 +0400

    1

---
+++ app/Providers/Backend/TimelineServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers\Backend;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimelineServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
