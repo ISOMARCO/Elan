40e923fb8e3786c10f8ed5e05e4573167d6e987d
ISOMARCO <inagiyev@icloud.com>
2024-02-20 09:54:47 +0400

    1

--- routes/Backend/Users/users.php
+++ routes/Backend/Users/users.php
@@ -3,8 +3,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Users\UsersController;
 use App\Http\Controllers\Backend\Users\TimelineController;
 
-Route::get('/admin/users', [UsersController::class, 'main'])->name("Backend_Users");
-Route::match(['post', 'get'], 'admin/users/saveChangesAction', [UsersController::class, 'saveChangesAction'])->name('Backend_SaveChangesAction');
-Route::match(['post', 'get'], '/admin/users/changeUserStatusAction', [UsersController::class, 'changeUserStatusAction'])->name('Backend_ChangeUserStatusAction');
-Route::get('/admin/users/timeline', [TimelineController::class, 'main'])->name('Backend_Users_Timeline');
-Route::get('/admin/users/deactive', [UsersController::class, 'deactive'])->name('Backend_Deactive_Users');
+Route::get('/users', [UsersController::class, 'main'])->name("Backend_Users");
+Route::match(['post', 'get'], '/users/saveChangesAction', [UsersController::class, 'saveChangesAction'])->name('Backend_SaveChangesAction');
+Route::match(['post', 'get'], '/users/changeUserStatusAction', [UsersController::class, 'changeUserStatusAction'])->name('Backend_ChangeUserStatusAction');
+Route::get('/users/timeline', [TimelineController::class, 'main'])->name('Backend_Users_Timeline');
+Route::get('/users/deactive', [UsersController::class, 'deactive'])->name('Backend_Deactive_Users');
