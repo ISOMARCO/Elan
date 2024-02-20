2de9107009929faf32a24063ce0b0dd66c147154
ISOMARCO <inagiyev@icloud.com>
2024-02-20 13:34:18 +0400

    1

--- routes/Backend/Authentication/authentication.php
+++ routes/Backend/Authentication/authentication.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Authentication\LoginController;
-Route::get('/admin/login', [LoginController::class, 'main'])->name("Backend_Login");
-Route::get('/admin/logout', [LoginController::class, 'logout'])->name('Backend_Logout');
-Route::match(['post', 'get'], 'admin/login/loginAction', [LoginController::class, 'loginAction'])->name('Backend_LoginAction');
+Route::get('/login', [LoginController::class, 'main'])->name("Backend_Login");
+Route::get('/logout', [LoginController::class, 'logout'])->name('Backend_Logout');
+Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction'])->name('Backend_LoginAction');
