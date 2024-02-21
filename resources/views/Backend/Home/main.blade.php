3bca113aa5dee6b5920ca44694f32c3010b3c981
ISOMARCO <inagiyev@icloud.com>
2024-02-21 10:04:13 +0400

    1

--- resources/views/Backend/Home/main.blade.php
+++ resources/views/Backend/Home/main.blade.php
@@ -15,7 +15,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">{{__('messages.home')}}</a></li>
+                        <li class="breadcrumb-item"><a href="#">{{__('Backend/messages.home')}}</a></li>
                         <li class="breadcrumb-item active">Blank Page</li>
                     </ol>
                 </div>
