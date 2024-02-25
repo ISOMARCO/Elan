03113d613da5669de3f8110f41284e58fda1f955
ISOMARCO <inagiyev@icloud.com>
2024-02-25 13:31:34 +0400

    1

--- resources/views/Backend/Users/deactive.blade.php
+++ resources/views/Backend/Users/deactive.blade.php
@@ -26,7 +26,6 @@
 <section class="content">
     <div class="card">
         <div class="card-header">
-            <button type="button" class="btn btn-primary" style="float: right;"><i class="fas fa-plus"></i></button>
         </div>
         <div class="card-body">
             @foreach($userList as $value)
