86612ca69a4fabf66cadbefe823a39d287236926
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-02-25 18:18:34 +0400

    1

--- app/Http/Controllers/Backend/Home/HomeController.php
+++ app/Http/Controllers/Backend/Home/HomeController.php
@@ -7,8 +7,6 @@ class HomeController extends Controller
 {
     public function main()
     {
-        $a = file_get_contents('https://monitoring.e-kassa.gov.az/#/index?doc=AobqSzhrcHDM');
-        echo json_encode($a, true);
         return view('Backend.Home.main');
     }
 }
