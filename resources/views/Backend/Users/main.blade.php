eb3acac56ee52254c6287e771878a0f544a29644
ISOMARCO <inagiyev@icloud.com>
2024-02-29 17:11:34 +0400

    1

--- resources/views/Backend/Users/main.blade.php
+++ resources/views/Backend/Users/main.blade.php
@@ -95,7 +95,7 @@
             var rowData = $(this).closest('.card').find('tbody td').map(function() {
                 return $(this).text();
             }).get();
-            alert("ok");
+            console.log("data: "+rowData);
             $("#user_edit_form #name").val(rowData[0]);
             $("#user_edit_form #surname").val(rowData[1]);
             $("#user_edit_form #email").val(rowData[2]);
