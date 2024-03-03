34bb0f9c744d3c88fa83b3b8c98f98cd432691cf
ISOMARCO <inagiyev@icloud.com>
2024-03-03 20:03:41 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::redirect('/home', '/');
 Route::match(['post', 'get'],'/telegram_webhook', [TelegramWebhookController::class, 'main'])->withoutMiddleware([VerifyCsrfToken::class]);
 Route::middleware([Backend_IsLogin::class])->get('/admin/', [BackendHomeController::class, 'main'])->name('Backend_Home');
 Route::redirect('/admin/home', '/admin/');
+Route::get('/pusher', function(){
+    return view('pusher');
+});
