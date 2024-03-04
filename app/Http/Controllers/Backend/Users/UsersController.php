e96f93d6734feee91e97aac129631785c13143dd
ISOMARCO <inagiyev@icloud.com>
2024-03-05 02:26:10 +0400

    1

--- app/Http/Controllers/Backend/Users/UsersController.php
+++ app/Http/Controllers/Backend/Users/UsersController.php
@@ -12,6 +12,7 @@ class UsersController extends Controller
 {
     public function main()
     {
+        echo "ok";
         $users = new Users();
         $userList = $users->allUsers();
         return view('Backend.Users.main', compact('userList'));
