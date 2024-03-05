3b994b2b56be4a93abfccf7d502a047d0daf8cc1
ISOMARCO <inagiyev@icloud.com>
2024-03-05 14:10:53 +0400

    1

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,8 +41,7 @@ return [
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-                'verify' => false
+                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https'
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
