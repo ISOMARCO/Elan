380a2ad5c7a90473bcffc56a0c5fe3309ed7ad51
ISOMARCO <inagiyev@icloud.com>
2024-03-06 13:24:17 +0400

    1

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Media\TelegramWebhookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
