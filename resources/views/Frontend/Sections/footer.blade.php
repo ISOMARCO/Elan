a65e4f9e32ee992a7fc5ee04fb719e7cc46816f8
ISOMARCO <inagiyev@icloud.com>
2024-03-06 19:40:37 +0400

    1

--- resources/views/Frontend/Sections/footer.blade.php
+++ resources/views/Frontend/Sections/footer.blade.php
@@ -172,8 +172,6 @@
         channel.bind('my-event', function(data) {
             $("#notification_count").html(parseInt($("#notification_count").text()) + 1);
             $("#notification_count1").html(parseInt($("#notification_count1").text()) + 1);
-            var audio = new Audio($("#notificationSound").attr("src"));
-            audio.play();
         });
     });
 </script>
