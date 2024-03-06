a660e77fd79debe5e8818aa35c763b01a7c79687
ISOMARCO <inagiyev@icloud.com>
2024-03-06 11:35:48 +0400

    1

--- app/Http/Controllers/Frontend/Home/HomeController.php
+++ app/Http/Controllers/Frontend/Home/HomeController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Frontend\Home;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
     public function main() : string
     {
-        echo "ok";
+        print_r(Session::all());
         return view('Frontend.Home.main');
     }
 }
