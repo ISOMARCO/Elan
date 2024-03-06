5166a02727db0760e630b8fc15852a589921990e
ISOMARCO <inagiyev@icloud.com>
2024-03-06 16:52:17 +0400

    1

--- routes/Backend/Authentication/authentication.php
+++ routes/Backend/Authentication/authentication.php
@@ -2,7 +2,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Authentication\LoginController;
 use App\Http\Middleware\VerifyCsrfToken;
+use App\Http\Middleware\Backend\IsLogin;
+
+
 Route::get('/login', [LoginController::class, 'main'])->name("Backend.Login");
 Route::get('/logout', [LoginController::class, 'logout'])->name('Backend.Logout');
 Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction'])->name('Backend.LoginAction');
-Route::match(['post', 'get'], '/api/login/loginAction', [LoginController::class, 'loginAction'])->name('Api.Backend.LoginAction')->withoutMiddleware([VerifyCsrfToken::class]);
+Route::match(['post', 'get'], '/api/login/loginAction', [LoginController::class, 'loginAction'])->name('Api.Backend.LoginAction')->withoutMiddleware([VerifyCsrfToken::class, IsLogin::class]);
