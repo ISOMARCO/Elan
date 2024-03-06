793dffda8f2919c4f63f42535d45c7e8d5b5e54b
ISOMARCO <inagiyev@icloud.com>
2024-03-06 11:28:35 +0400

    1

--- routes/Frontend/Authentication/authentication.php
+++ routes/Frontend/Authentication/authentication.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Frontend\Authentication\LoginController;
 use App\Http\Controllers\Frontend\Authentication\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [RegisterController::class, 'main'])->name('Register');
+Route::get('/register', [RegisterController::class, 'main'])->name('Frontend.Register');
 Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction'])->name("Frontend.LoginAction");
-Route::get('/login', [LoginController::class, 'main'])->name('Login');
+Route::get('/login', [LoginController::class, 'main'])->name('Frontend.Login');
 Route::match(['post', 'get'], '/register/registerAction', [RegisterController::class, 'registerAction'])->name('Frontend.RegisterAction');
-Route::get('/logout', [LoginController::class, 'logout'])->name('Logout');
+Route::get('/logout', [LoginController::class, 'logout'])->name('Frontend.Logout');
--- routes/Frontend/User/user.php
+++ routes/Frontend/User/user.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Frontend\User\SettingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user_setting', [SettingController::class, 'main'])->name('user_setting');
+Route::get('/user_setting', [SettingController::class, 'main'])->name('Frontend.UserSetting');
