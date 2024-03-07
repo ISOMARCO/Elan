92439f1a350bcb76d1d70617df1b28b4a227eb8b
ISOMARCO <inagiyev@icloud.com>
2024-03-07 13:45:48 +0400

    1

--- routes/Backend/Authentication/api.php
+++ routes/Backend/Authentication/api.php
@@ -1,8 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Authentication\LoginController;
-use App\Http\Middleware\VerifyCsrfToken;
-use App\Http\Middleware\Backend\IsLogin;
 
 
 Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction'])->name('Api.Backend.LoginAction');
