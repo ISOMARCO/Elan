adf13a07629d9b7e0b23000e4ffafe7b5b4c2fcc
ISOMARCO <inagiyev@icloud.com>
2024-03-07 13:46:43 +0400

    1

--- routes/Backend/Authentication/api.php
+++ routes/Backend/Authentication/api.php
@@ -3,4 +3,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Authentication\LoginController;
 
 
-Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction'])->name('Api.Backend.LoginAction');
+Route::match(['post', 'get'], '/login/loginAction', [LoginController::class, 'loginAction'])->name('Backend.Api.LoginAction');
