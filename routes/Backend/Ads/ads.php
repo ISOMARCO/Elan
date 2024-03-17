efcdf80f0186493783fe6ebafa6161bd17c62249
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-18 02:53:22 +0400

    1

--- routes/Backend/Ads/ads.php
+++ routes/Backend/Ads/ads.php
@@ -1,5 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Ads\CategoryController;
+use App\Http\Controllers\Backend\Ads\CategorySettingsController;
 
 Route::get('/category', [CategoryController::class, 'main'])->name("Backend.Ads_Category");
+Route::get('/categorySettings', [CategorySettingsController::class, 'main'])->name('Backend.Ads_CategorySettings');
