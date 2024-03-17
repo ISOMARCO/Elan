602bf3c5149030241eb2ef7ad1206b374aa8acd8
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-18 02:30:56 +0400

    1

---
+++ app/Http/Controllers/Backend/Ads/CategorySettingsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Ads;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategorySettingsController extends Controller
+{
+    public function main()
+    {
+
+    }
+}
