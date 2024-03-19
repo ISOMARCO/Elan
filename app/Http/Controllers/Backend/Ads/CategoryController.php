16653e5b5e395d93827cf26beeffb07a87b0139e
ISOMARCO <inagiyev@icloud.com>
2024-03-19 21:47:31 +0400

    1

--- app/Http/Controllers/Backend/Ads/CategoryController.php
+++ app/Http/Controllers/Backend/Ads/CategoryController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Backend\Ads;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Ads\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function main()
+    public function main(Request $request)
     {
-        return view('Backend.Ads.category');
+        $category = new Category();
+        $allCategory = $category->allCategory();
+        return view('Backend.Ads.category', compact('allCategory'));
     }
 
 }
--- app/Models/Backend/Ads/Category.php
+++ app/Models/Backend/Ads/Category.php
@@ -4,8 +4,14 @@ namespace App\Models\Backend\Ads;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Category extends Model
 {
     use HasFactory;
+    protected $table = 'Category';
+
+    public function allCategory()
+    {
+        return DB::table($this->table)->get();
+    }
 }
