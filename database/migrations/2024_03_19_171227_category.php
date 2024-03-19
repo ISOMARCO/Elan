d2c627d39d76838c2f7d9ec0c4e47ce3a9a51f72
ISOMARCO <inagiyev@icloud.com>
2024-03-19 21:20:44 +0400

    1

--- database/migrations/2024_03_19_171227_category.php
+++ database/migrations/2024_03_19_171227_category.php
@@ -9,9 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    protected $table = 'Category';
     public function up(): void
     {
-        //
+        if(!Schema::hasTable($this->table))
+        {
+            Schema::create($this->table, function (Blueprint $table) {
+                $table->bigIncrements('Id');
+                $table->string('Name', 200)->nullable();
+                $table->integer('Parent')->nullable();
+                $table->integer('Main_Category')->nullable();
+                $table->smallInteger('Level')->default('1');
+                $table->string('Status', 10)->default('ACTIVE');
+                $table->timestamp('Created_Date')->nullable();
+            });
+        }
     }
 
     /**
