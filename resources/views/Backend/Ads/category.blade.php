1695c8677644360c873691afe3a67a168c65e263
ISOMARCO <inagiyev@icloud.com>
2024-03-19 11:04:11 +0400

    1

--- resources/views/Backend/Ads/category.blade.php
+++ resources/views/Backend/Ads/category.blade.php
@@ -66,6 +66,17 @@
                             </div>
                         </div>
                     </div>
+                    <div class="card-body">
+
+                        Body olacaq
+
+
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary" style="float:left">Edit&nbsp;<i class="fas fa-edit"></i></button>
+                        <button type="button" class="btn btn-danger" style="float:right" onclick="return confirm('Are you sure?')">Delete&nbsp;<i class="fas fa-trash"></i></button>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
