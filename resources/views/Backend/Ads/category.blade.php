64bc15a20acf43a3b0539129752665eec62b5242
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-21 15:51:03 +0400

    1

--- resources/views/Backend/Ads/category.blade.php
+++ resources/views/Backend/Ads/category.blade.php
@@ -2,11 +2,7 @@
 <html lang="en">
 <head>
     @include('Backend.Sections.head')
-    <link rel="stylesheet" href="{{asset('Assets/Backend/plugins/select2/css/select2.min.css')}}">
     <link rel="stylesheet" href="{{asset('Assets/Backend/plugins/select2bs4/select2-bootstrap4.min.css')}}">
-    <style>
-        #parent {width:100%!important;}
-    </style>
 </head>
 <body class="hold-transition sidebar-mini">
     @include('Backend.Sections.header')
@@ -64,7 +60,6 @@
         @include('Backend.Ads.category_add')
     </section>
     @include('Backend.Sections.footer')
-    <script src="{{asset('Assets/Backend/plugins/select2/js/select2.js')}}"></script>
     <script>
         $(document).ready(function(){
             $("#parent").select2();
