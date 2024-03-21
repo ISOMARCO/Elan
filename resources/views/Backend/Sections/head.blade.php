2ea0fd341d040afa787bb965ddf517ebf5eb5371
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-21 18:03:48 +0400

    1

--- resources/views/Backend/Sections/head.blade.php
+++ resources/views/Backend/Sections/head.blade.php
@@ -6,8 +6,8 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome -->
 <link rel="stylesheet" href="{{asset('Assets/Backend/plugins/fontawesome-free/css/all.min.css')}}">
+<link rel="stylesheet" href="{{asset('Assets/Backend/plugins/fontawesome6.5.1/css/all.min.css')}}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{{asset('Assets/Backend/css/adminlte.min.css')}}">
 <!-- Sweet alert2 -->
 <link rel="stylesheet" href="{{asset('Assets/Backend/plugins/sweetalert2/sweetalert2.min.css')}}">
-<link rel="stylesheet" href="{{asset('Assets/Backend/plugins/fontawesome6.5.1/css/all.min.css')}}">
