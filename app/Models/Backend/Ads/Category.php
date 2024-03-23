b1cb42b019f6a1d02967315aadef3b25d4cb0bba
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-23 19:57:02 +0400

    1

--- app/Models/Backend/Ads/Category.php
+++ app/Models/Backend/Ads/Category.php
@@ -9,9 +9,28 @@ class Category extends Model
 {
     use HasFactory;
     protected $table = 'Category';
+    protected int|NULL $id = NULL;
+    protected string|NULL $name = NULL;
+    protected int|NULL $parent = NULL;
+    protected int|NULL $mainCategory = NULL;
+    protected int $level = 1;
+    protected string $status = 'ACTIVE';
+    protected string|NULL $photo = NULL;
+    protected string|NULL $createdDate = NULL;
 
-    public function allCategory()
+    public function __call($method, $args=[]) : Category
+    {
+        $this->$method = $args[0];
+        return $this;
+    }
+
+    public function allCategory() : Array
     {
         return DB::table($this->table)->where('Level', 1)->get();
+    }
+
+    public function createCategory() : bool
+    {
+        return true;
     }
 }
