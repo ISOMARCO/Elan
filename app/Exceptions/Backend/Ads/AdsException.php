3c1a4dfda3df042aa0a54e596b0c5bf4d0642f7b
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-23 19:38:27 +0400

    1

--- app/Exceptions/Backend/Ads/AdsException.php
+++ app/Exceptions/Backend/Ads/AdsException.php
@@ -6,5 +6,38 @@ use Exception;
 
 class AdsException extends Exception
 {
-    //
+    protected $code = 500;
+    public function __construct(int $statusCode = 500, Array $message = [])
+    {
+        $this->code = $statusCode;
+        if(is_array($message)  || empty($message))
+        {
+            $message = $this->errorCodeMessage($message);
+        }
+        parent::__construct($message, $statusCode);
+    }
+
+    protected function errorCodeMessage(Array $msg = []) : String|NULL
+    {
+        $codes = [
+
+        ];
+        if(isset($codes[$this->code]))
+        {
+            return $codes[$this->code];
+        }
+        return NULL;
+    }
+
+    protected function reportErrorCodes() : String|NULL
+    {
+        $codes = [
+
+        ];
+        if(isset($codes[$this->code]))
+        {
+            return $codes[$this->code];
+        }
+        return NULL;
+    }
 }
