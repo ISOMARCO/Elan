f0979b70c3e125b6bd66af11e81f27eeb4858501
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-23 20:10:18 +0400

    1

--- app/Http/Controllers/Backend/Ads/CategoryController.php
+++ app/Http/Controllers/Backend/Ads/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Backend\Ads;
 
+use App\Exceptions\Backend\Ads\AdsException;
 use App\Http\Controllers\Controller;
 use App\Models\Backend\Ads\Category;
 use Illuminate\Http\Request;
@@ -13,6 +14,23 @@ class CategoryController extends Controller
         $category = new Category();
         $allCategory = $category->allCategory();
         return view('Backend.Ads.category', compact('allCategory'));
+    }
+
+    public function createAction(Request $request)
+    {
+        if($request->ajax() || $request->wantsJson())
+        {
+            $category = new Category();
+            try
+            {
+                $category->name($request->post('name'))->parent($request->post('parent'));
+            }
+            catch(AdsException $e)
+            {
+                return response()->json(['error' => $e->getMessage()], 500);
+            }
+        }
+        abort(403, 'Unauthorized');
     }
 
 }
