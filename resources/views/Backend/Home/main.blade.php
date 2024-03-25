2718873e5a39ef8de41a705b39306325dc6a53e6
ISOMARCO <inagiyev@icloud.com>
2024-03-25 11:20:37 +0400

    1

--- resources/views/Backend/Home/main.blade.php
+++ resources/views/Backend/Home/main.blade.php
@@ -31,7 +31,7 @@
                 <h3 class="card-title">Title</h3>
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fa-solid fa-minus"></i>
+                        <i class="fas fa-minus"></i>
                     </button>
                     <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
                         <i class="fa-solid fa-xmark"></i>
