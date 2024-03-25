28b323b88e2019d480c720ae1869332c4ea5fd8a
ISOMARCO <inagiyev@icloud.com>
2024-03-25 11:17:34 +0400

    1

--- resources/views/Backend/Sections/footer.blade.php
+++ resources/views/Backend/Sections/footer.blade.php
@@ -2,7 +2,7 @@
 </div>
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-        <b>Version</b> 0.0.1
+        <b>Version</b> 0.0.2
     </div>
     <strong>Copyright &copy; 2014-2021 <a href="#">ISOMARCO</a>.</strong> All rights reserved.
 </footer>
