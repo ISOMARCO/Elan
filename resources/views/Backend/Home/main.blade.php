1a9be920ece960902af74514a9a308e83a4dcf12
ISOMARCO <inagiyev@icloud.com>
2024-03-25 11:16:11 +0400

    1

--- resources/views/Backend/Home/main.blade.php
+++ resources/views/Backend/Home/main.blade.php
@@ -29,7 +29,6 @@
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">Title</h3>
-
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                         <i class="fa-solid fa-minus"></i>
