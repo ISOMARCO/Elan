3908429ebbf6c308d288799ee2f1ee34ea35fe07
ISOMARCO <inagiyev@icloud.com>
2024-03-27 23:32:08 +0400

    1

--- resources/views/Backend/Money_Manager/goods.blade.php
+++ resources/views/Backend/Money_Manager/goods.blade.php
@@ -114,7 +114,7 @@
                     ++countResults;
                     lastResult = decodedText;
                     $("#goods_add_form #barcode").val(decodedResult.decodedText);
-                    html5QrcodeScanner.stop();
+                    //html5QrcodeScanner.stop();
                 }
 
             }
