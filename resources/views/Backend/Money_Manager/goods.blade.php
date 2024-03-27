1b012ba776dc0cd67e89f537b0fae822d6001af9
ISOMARCO <inagiyev@icloud.com>
2024-03-27 11:56:58 +0400

    1

--- resources/views/Backend/Money_Manager/goods.blade.php
+++ resources/views/Backend/Money_Manager/goods.blade.php
@@ -21,7 +21,30 @@
     </div><!-- /.container-fluid -->
 </section>
 <section class="content">
-    content
+    <div class="card">
+        <div class="card-header">
+            <button type="button" class="btn btn-primary" style="float: right;" data-toggle="modal" data-target="#goods_add"><i class="fa-solid fa-plus"></i></button>
+        </div>
+        <div class="card-body">
+            <div class="card collapsed-card" style="border: 1px solid {{Border_Random_Color()}}">
+                <div class="card-header">
+                    <h3 class="card-title">Sud</h3>
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                            <i class="fa-solid fa-plus"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body" style="overflow: auto; white-space: nowrap;">
+
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-outline-secondary" style="float: left;" data-toggle="modal" data-target="#user_edit"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button type="button" class="btn btn-outline-danger" style="float: right;" data-toggle="modal" data-target="#user_ban"><i class="fa-solid fa-trash-can"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @include('Backend.Sections.footer')
 </body>
