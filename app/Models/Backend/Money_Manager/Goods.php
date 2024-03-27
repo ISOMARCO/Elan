0f72539121327c332896965092188a6c318da72a
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-27 22:15:07 +0400

    1

--- app/Models/Backend/Money_Manager/Goods.php
+++ app/Models/Backend/Money_Manager/Goods.php
@@ -4,8 +4,12 @@ namespace App\Models\Backend\Money_Manager;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
 
 class Goods extends Model
 {
     use HasFactory;
+    public function allGoods()
+    {
+    }
 }
