fa8adf65dca57222cfbee04085af75fe7ae69e35
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-03-27 21:10:24 +0400

    1

--- app/Exceptions/Backend/Money_Manager/MoneyManagerException.php
+++ app/Exceptions/Backend/Money_Manager/MoneyManagerException.php
@@ -6,5 +6,38 @@ use Exception;
 
 class MoneyManagerException extends Exception
 {
-    //
+    protected $code = 500;
+    public function __construct(int $statusCode = NULL, string $message = NULL)
+    {
+        $this->code = $statusCode;
+        if($message == NULL)
+        {
+            $message = $this->errorCodeMessage();
+        }
+        parent::__construct($message, $statusCode);
+    }
+
+    protected function errorCodeMessage() : String
+    {
+        $codes = [
+            '4000'
+        ];
+        if(isset($codes[$this->code]))
+        {
+            return $codes[$this->code];
+        }
+        return "ERROR";
+    }
+
+    protected function reportErrorCodes() : String
+    {
+        $codes = [
+            '5000'
+        ];
+        if(isset($codes[$this->code]))
+        {
+            return $codes[$this->code];
+        }
+        return "";
+    }
 }
