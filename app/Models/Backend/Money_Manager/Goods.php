2c1c03bcef4ed9ebe4e82408d9722200867ea974
root <root@vmi1570124.contaboserver.net>
2024-03-27 22:12:06 +0400

    1

---
+++ app/Models/Backend/Money_Manager/Goods.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Backend\Money_Manager;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Goods extends Model
+{
+    use HasFactory;
+}
