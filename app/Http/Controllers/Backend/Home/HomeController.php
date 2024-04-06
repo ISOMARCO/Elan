7c5299d3d9c344a07040564ef4fa1e9c6b7b8902
ISOMARCO <inagiyev@icloud.com>
2024-04-06 14:53:36 +0400

    1

--- app/Http/Controllers/Backend/Home/HomeController.php
+++ app/Http/Controllers/Backend/Home/HomeController.php
@@ -7,6 +7,46 @@ class HomeController extends Controller
 {
     public function main()
     {
+        $url = "https://sandbox.booknetic.com/sandboxes/sandbox-saas-6f49ae724d32a0cf3823/wp-admin/admin-ajax.php";
+
+// POST verisi
+        $data = array(
+            'location' => -1,
+            'staff' => -1,
+            'service_category' => '',
+            'service' => 13,
+            'service_extras' => [],
+            'date' => '',
+            'time' => '',
+            'brought_people_count' => 0,
+            'recurring_start_date' => '',
+            'recurring_end_date' => '',
+            'recurring_times' => "{}",
+            'appointments' => "[]",
+            'customer_data' => (object) array()
+        );
+
+// curl başlat
+        $ch = curl_init();
+
+// curl ayarları
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen(json_encode($data)))
+        );
+
+// isteği yap ve cevabı al
+        $response = curl_exec($ch);
+
+// curl bağlantısını kapat
+        curl_close($ch);
+
+// cevabı ekrana yazdır
+        echo $response;
         return view('Backend.Home.main');
     }
 }
